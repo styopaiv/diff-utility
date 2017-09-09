199309938df7b70c4c70457325ac1fd6073a6a59
Stepan <aastralplane@gmail.com>
2017-09-10 03:20:18 +1100

    parser fix

--- src/parseConfig.js
+++ src/parseConfig.js
@@ -1,15 +1,12 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parseOptions = new Map();
-const [jsonExt, ymlExt, yamlExt, iniExt] = ['.json', '.yml', '.yaml', '.ini'];
-
 export default (extension, file) => {
-  parseOptions.set(jsonExt, arg => JSON.parse(arg));
-  parseOptions.set(ymlExt, arg => yaml.load(arg));
-  parseOptions.set(yamlExt, arg => yaml.load(arg));
-  parseOptions.set(iniExt, arg => ini.parse(arg));
-
-  const parse = parseOptions.get(extension);
-  return parse(file);
+  const parsers = {
+    '.json': arg => JSON.parse(arg),
+    '.yml': arg => yaml.load(arg),
+    '.yaml': arg => yaml.load(arg),
+    '.ini': arg => ini.parse(arg),
+  };
+  return parsers[extension](file);
 };
